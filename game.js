7297d2559e1b59c4b38b27be70d790c3233cc48c
Ramshaa10 <58940309+Ramshaa10@users.noreply.github.com>
2020-12-25 18:12:35 +0100

    stand 3 bewegugn geht

--- game.js
+++ game.js
@@ -1,9 +1,8 @@
-let leftPaddleX = 50; // Position von x-achse des linken Paddle
-let leftPaddleY = 20; // Position von y-achse des linken Paddle
-let rightPaddleX = 500; // Position von von x-achse des rechten Paddle
+let leftPaddleX = 10; // Position von x-achse des linken Paddle
+let leftPaddleY = 150; // Position von y-achse des linken Paddle
+let rightPaddleX = 300; // Position von von x-achse des rechten Paddle
 let rightPaddleY = 20;
-let speedX1 = 0;
-let speedX2 = 0;
+let speed = 0;
 function game() {
   fill("pink");
   rect(leftPaddleX, leftPaddleY, 20, 100);
@@ -12,31 +11,29 @@ function game() {
 }
 
 function movement() {
+  // arrow up, nach oben linker Paddle
   if (keyIsDown(87)) {
-    speedX1 -= 5;
-  } // arrow up, nach oben linker Paddle
+    leftPaddleY -= speed + 15;
+  }
 
   // arrow down, nach unten linker Paddle
   else if (keyIsDown(83)) {
-    speedX1 += 5;
+    leftPaddleY += speed + 15;
   }
   // w tase nach oben rechter Paddel
   if (keyIsDown(38)) {
-    speedX2 -= 5;
+    rightPaddleY -= speed + 15;
   }
   // s taste nach unten rechter Paddel
   else if (keyIsDown(40)) {
-    speedX2 += 5;
+    rightPaddleY += speed + 15;
   }
 }
-
 function draw() {
+  clear();
+  noStroke();
   game();
   movement();
-  speedX1 = speedX1 + 1;
-  speedX2 = speedX2 + 1;
-
-  // Rechnung aufgestellt, also er soll es immer erhöhen
 }
-leftPaddleY = leftPaddleX + speedX1;
-rightPaddleY = rightPaddleX + speedX2;
+// Rechnung aufgestellt, also er soll es immer erhöhen
+speed += speed;
