86eff66f0ba08b75ea888d9a5cc593640a029fa9
Ramshaa10 <58940309+Ramshaa10@users.noreply.github.com>
2020-12-26 16:04:35 +0100

    funktionen durch objekte ersetzt->bessere Struktur

--- game.js
+++ game.js
@@ -1,42 +1,37 @@
-let leftPaddleX = 10; // Position von x-achse des linken Paddle
-let leftPaddleY = 150; // Position von y-achse des linken Paddle
-let rightPaddleX = 550; // Position von von x-achse des rechten Paddle
-let rightPaddleY = 20;
 let speed = 0;
+let ballSpeedX = 10;
+let ballSpeedY = 5;
 let score = [];
+let ball = {
+  x: 300,
+  y: 100,
+};
+let rightPaddle = {
+  x: 550,
+  y: 20,
+};
+let leftPaddle = {
+  x: 20,
+  y: 150,
+};
 
-function game() {
-  fill("pink");
-  rect(leftPaddleX, leftPaddleY, 20, 100);
-  fill("lightblue");
-  rect(rightPaddleX, rightPaddleY, 20, 100);
-  fill("white");
-  //   for-schleife für die Trennwand
-  for (let i = 0; i < 5; i++) {
-    rect(300, 10 + i * 130, 10, 50 + i * 5);
-  }
-}
-function ball() {
-  fill("white");
-  ellipse(300, 100, 20, 20);
-}
 function movement() {
   // arrow up, nach oben linker Paddle
   if (keyIsDown(87)) {
-    leftPaddleY -= speed + 15;
+    leftPaddle.y -= speed + 15;
   }
 
   // arrow down, nach unten linker Paddle
   else if (keyIsDown(83)) {
-    leftPaddleY += speed + 15;
+    leftPaddle.y += speed + 15;
   }
   // w tase nach oben recswshter Paddel
   if (keyIsDown(38)) {
-    rightPaddleY -= speed + 15;
+    rightPaddle.y -= speed + 15;
   }
   // s taste nach unten rechter Paddel
   else if (keyIsDown(40)) {
-    rightPaddleY += speed + 15;
+    rightPaddle.y += speed + 15;
   }
 }
 
@@ -44,9 +39,20 @@ function draw() {
   clear();
   background("black");
   noStroke();
-  game();
   movement();
-  ball();
+  ellipse(ball.x, ball.y, 20, 20);
+  fill("pink");
+  rect(leftPaddle.x, leftPaddle.y, 20, 100);
+  fill("lightblue");
+  rect(rightPaddle.x, rightPaddle.y, 20, 100);
+  //   for-schleife für die Trennwand
+  fill("white");
+  for (let i = 0; i < 5; i++) {
+    rect(300, 10 + i * 130, 10, 50 + i * 5);
+  }
 }
 // Rechnung aufgestellt, also er soll es immer erhöhen
-speed += speed;
+function calculate() {
+  speed += speed;
+}
+calculate();
