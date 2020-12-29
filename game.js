5b04e6b2c1ea401d1bff1fc1190144e519986960
Ramshaa10 <58940309+Ramshaa10@users.noreply.github.com>
2020-12-29 14:17:22 +0100

    29.12
    screen einbau bzw menü aufbau

--- game.js
+++ game.js
@@ -1,16 +1,75 @@
 let speed = 0;
 let ballSpeedX = 10;
 let ballSpeedY = 5;
-let score = [];
+let scoree = 0;
+let highScoree = 0;
+let startt = true;
+let lostt = false;
+let helpp = false;
+let newHighScore = false;
 
+// function preload() {}
+function help() {
+  textSize(16);
+  fill(252, 227, 255);
+
+  text("help", 275, 610);
+
+  if (helpp === true) {
+    fill("green");
+    ellipse(200, 200, 10, 10);
+  }
+}
+
+function start() {
+  if (startt === true) {
+    fill("red");
+    ellipse(300, 100, 40, 40);
+    textFont("skia");
+    noStroke();
+    fill(29, 17, 89);
+    textSize(35);
+    textStyle(BOLD);
+    text("Start", 254, 556);
+  }
+}
+
+function youlost() {
+  if (lostt === true) {
+    fill("yellow");
+    rect(400, 200, 50, 50);
+  }
+}
 function design() {
   //schwarzer Kasten
   fill("black");
   rect(0, 0, 1500, 1500);
-  // for-schleife für die Trennwand
+
+  //for-schleife für die Trennwand
   fill("white");
   for (let i = 0; i < 5; i++) {
     rect(300, 10 + i * 130, 10, 50 + i * 5);
+  }
+}
+
+function score() {
+  noStroke();
+  textFont("skia");
+  textSize(40);
+  fill(21, 129, 153);
+  text("score: " + scoree, 100, 50);
+}
+
+function highscore() {
+  noStroke();
+  textFont("skia");
+  textSize(40);
+  fill(21, 129, 153);
+  text("highscore: " + highscoree, 280, 50);
+
+  if (newHighScore === true && lostt === true) {
+    fill("blue");
+    ellipse(359, 110, 45, 20);
   }
 }
 let ball = {
@@ -27,6 +86,8 @@ let leftPaddle = {
 };
 
 function ballMovement() {
+  // bei den Winkel Hilfe entnommen von: https://www.youtube.com/watch?v=nl0KXCa5pJk
+
   //Rechnung aufgestellt
   ball.x += ballSpeedX;
   ball.x -= ballSpeedX;
@@ -39,7 +100,6 @@ function movement() {
   if (keyIsDown(87)) {
     leftPaddle.y -= speed + 15;
   }
-
   // arrow down, nach unten linker Paddle
   else if (keyIsDown(83)) {
     leftPaddle.y += speed + 15;
@@ -68,4 +128,40 @@ function draw() {
   rect(leftPaddle.x, leftPaddle.y, 20, 100);
   fill("lightblue");
   rect(rightPaddle.x, rightPaddle.y, 20, 100);
+  fill("white");
+  if (startt == true) {
+    if (
+      mouseIsPressed === true &&
+      mouseX > 250 &&
+      mouseX < 350 &&
+      mouseY > 520 &&
+      mouseY < 570
+    ) {
+      //Start
+      startt = false;
+      lostt = false;
+      newHighScore = false;
+    }
+  }
+
+  if (
+    mouseIsPressed === true &&
+    mouseX > 270 &&
+    mouseX < 330 &&
+    mouseY > 590 &&
+    mouseY < 620
+  ) {
+    //help
+    helpp = true;
+  }
+}
+
+function mousePressed() {
+  if (help) {
+    helpb = false;
+  }
+
+  if (lost) {
+    lostt = false;
+  }
 }
