3460472d7e0023f7c5b7c73d66d7cfedba1dc6eb
Ramshaa10 <58940309+Ramshaa10@users.noreply.github.com>
2021-02-08 22:27:21 +0100

    stand 08.02

---
+++ Pong.js
@@ -0,0 +1,266 @@
+var fieldX = 200;
+var fieldY = 100;
+var fieldW = 600;
+var fieldH = 400;
+
+var leftPaddleX = 20;
+var leftPaddleY = 150;
+var leftPaddleW = 20;
+var leftPaddleH = 100;
+var BallSpeed = 0;
+
+var rightPaddleX = 560;
+var rightPaddleY = 150;
+var rightPaddleW = 20;
+var rightPaddleH = 100;
+
+var Ballx = fieldW / 2 + fieldX;
+var Bally = fieldH / 2 + fieldY;
+var BallD = 5;
+
+var leftPaddlePoint = 0;
+var rightPaddlePoint = 0;
+var BallDir = 0;
+var BallXSpeed = BallSpeed;
+var BallYSpeed = BallSpeed;
+
+var resetButtonX = 550;
+var resetButtonY = 700;
+var resetButtonW = 150;
+var resetButtonH = 50;
+
+var helpButtonX = 250;
+var helpButtonY = 700;
+var helpButtonW = 150;
+var helpButtonH = 50;
+
+var luigi;
+var mario;
+var pilz;
+var rasen;
+
+var help = false;
+var start = true;
+var end = false;
+
+function setup() {
+  let canvas = createCanvas(1000, 800);
+  canvas.parent("Pong");
+}
+
+// fieldscreen = loadImage("Bilder/rasen.jpg");
+mario = loadImage("Bilder/supermario.jpg");
+luigi = loadImage("Bilder/luigi.jpg");
+pilz = loadImage("Bilder/pilz.jpg");
+
+function Score() {
+  textSize(20);
+  fill(255);
+  strokeWeight(1);
+  textFont("italic");
+  text(leftPaddlePoint, fieldX + 100, fieldY + 450);
+  text(rightPaddlePoint, fieldX + 500, fieldY + 450);
+
+  if (Ballx < 20 + fieldX) {
+    rightPaddlePoint += 1;
+  }
+  if (Ballx > 580 + fieldX) {
+    leftPaddlePoint += 1;
+  }
+}
+function helpButton() {
+  textSize(30);
+  textFont("italic");
+  fill(252, 223, 163);
+  rect(helpButtonX, helpButtonY, helpButtonW, helpButtonH);
+  fill(73, 52, 51);
+  text("Help", helpButtonX + helpButtonW / 3, helpButtonY + helpButtonH / 1.5);
+
+  if (help) {
+    start = false;
+    fill(223, 160, 85);
+    rect(350, 140, 300, 300);
+    fill("black");
+    textSize(15);
+    text(
+      "Verwende die Pfeiltasten nach oben,\n und unten um Mario zu bewegen.\n Um Luigi zu bewegen verwende die Tasten\n W und S.",
+      360,
+      250
+    );
+  }
+}
+
+function field() {
+  fill("black");
+  strokeWeight(10);
+  stroke(168, 157, 131);
+  rect(0, 0, 1000, 800);
+  fill(195, 38, 22);
+  rect(fieldX, fieldY, fieldW, fieldH, 30);
+  stroke(0);
+  strokeWeight(2);
+}
+
+function leftPaddle() {
+  //movement
+  image(
+    luigi,
+    leftPaddleX + fieldX,
+    leftPaddleY + fieldY,
+    leftPaddleW,
+    leftPaddleH
+  );
+
+  //Up
+  if (leftPaddleY > 20 && keyIsDown(87)) {
+    leftPaddleY = leftPaddleY - 10;
+  }
+  //Down
+  if (leftPaddleY < 280 && keyIsDown(83)) {
+    leftPaddleY = leftPaddleY + 10;
+  }
+}
+if (
+  Ballx > leftPaddleX - leftPaddleW + fieldX + BallD / 2 &&
+  Ballx < leftPaddleX + leftPaddleW + fieldX + BallD / 2 &&
+  Bally < leftPaddleY + leftPaddleW + fieldY + BallD / 2 &&
+  Bally > leftPaddleY + fieldY + BallD / 2
+) {
+  BallDir += 1;
+}
+
+function rightPaddle() {
+  //movement
+  image(
+    mario,
+    rightPaddleX + fieldX,
+    rightPaddleY + fieldY,
+    rightPaddleW,
+    rightPaddleH
+  );
+  //W up
+  if (rightPaddleY > 20 && keyIsDown(38)) {
+    rightPaddleY = rightPaddleY - 10;
+  }
+  //S down
+  if (rightPaddleY < 280 && keyIsDown(40)) {
+    rightPaddleY = rightPaddleY + 10;
+  }
+}
+
+function ball() {
+  image(pilz, Ballx - 15, Bally - 15, 30, 30);
+  if (start) {
+    //   Ball move
+    Ballx = Ballx + BallXSpeed;
+    Bally = Bally + BallYSpeed;
+
+    if (
+      Ballx < 0 + fieldX + BallD / 2.5 ||
+      Ballx > fieldW + fieldX - BallD / 2.5
+    ) {
+      Ballx = fieldW / 2 + fieldX;
+      Bally = fieldH / 2 + fieldY;
+      BallXSpeed *= -1;
+    }
+
+    if (
+      Bally < 10 + fieldY + BallD / 2.5 ||
+      Bally > 390 + fieldY - BallD / 2.5
+    ) {
+      BallYSpeed *= -1;
+    }
+    edges(leftPaddleX, leftPaddleY, leftPaddleW, leftPaddleH);
+    edges(rightPaddleX, rightPaddleY, rightPaddleW, rightPaddleH);
+  }
+
+  function edges(X, Y, sizeX, sizeY) {
+    if (
+      Ballx > X - sizeX + fieldX + BallD / 2 &&
+      Ballx < X + sizeX + fieldX + BallD / 2 &&
+      Bally < Y + sizeY + fieldY + BallD / 2 &&
+      Bally > Y + fieldY + BallD / 2
+    ) {
+      BallXSpeed *= -1;
+      var pad = Bally - fieldY - Y;
+      if (pad > 45 && pad < 55) {
+        BallSpeed *= -1;
+      } else {
+        if (BallSpeed < 0) {
+          BallSpeed *= -1;
+        } else {
+          BallSpeed *= 1;
+        }
+      }
+    }
+  }
+}
+
+function resetButton() {
+  fill(252, 223, 163);
+  rect(resetButtonX, resetButtonY, resetButtonW, resetButtonH);
+  fill(73, 52, 51);
+  noStroke();
+  textFont("italic");
+  textSize(30);
+  text(
+    "Start",
+    resetButtonX + resetButtonW / 3,
+    resetButtonY + resetButtonH / 1.5
+  );
+
+  if (
+    mouseX > resetButtonX &&
+    mouseX < resetButtonX + resetButtonW &&
+    mouseY > resetButtonY &&
+    mouseY < resetButtonY + resetButtonH
+  ) {
+    if (mouseIsPressed) {
+      Ballx = fieldW / 2 + fieldX;
+      BallXSpeed = 10;
+      BallYSpeed = 10;
+      leftPaddlePoint = 0;
+      rightPaddlePoint = 0;
+      BallDir = 0;
+      help = false;
+      start = true;
+    }
+  }
+}
+
+function draw() {
+  clear();
+  field();
+  leftPaddle();
+  rightPaddle();
+  ball();
+  Score();
+  resetButton();
+  helpButton();
+  gameOver();
+
+  if (
+    mouseIsPressed &&
+    mouseX > helpButtonX &&
+    mouseX < helpButtonX + helpButtonW &&
+    mouseY > helpButtonY &&
+    mouseY < helpButtonY + helpButtonH
+  ) {
+    //help Button
+    help = true;
+  }
+}
+function gameOver() {
+  if (rightPaddlePoint === 5 || leftPaddlePoint === 5) {
+    BallXSpeed = 0;
+    BallYSpeed = 0;
+    rightPaddlePoint = 0;
+    leftPaddlePoint = 0;
+    fill("white");
+    rect(300, 300, 300, 300);
+    text("You lost", 150, 150);
+
+    start = false;
+    endscreen = true;
+  }
+}
---
+++ SnakeSchwer.js
@@ -0,0 +1,263 @@
+let snakex = [300];
+let snakey = [300];
+let direction = 0; //zuweisung für Richtung/ Bewegung
+let score = 0;
+let highscore = 0;
+let time = 0; //zuweisung für "Zeit" bis zur nächsten Bewegung
+let zfaster = 0;
+let begin = true;
+let newfood = true;
+let lost = false;
+let newhighscore = false;
+let help = false;
+let foodx;
+let foody;
+let pox;
+let poy;
+var mario;
+var pilz;
+
+function setup() {
+  let canvas = createCanvas(600, 620);
+  canvas.parent("SnakeSchwer");
+}
+mario = loadImage("Bilder/supermario.jpg");
+pilz = loadImage("Bilder/pilz.jpg");
+
+function spielfeld() {
+  stroke(255);
+  line(100, 100, 500, 100);
+  line(500, 100, 500, 500);
+  line(500, 500, 100, 500);
+  line(100, 500, 100, 100);
+}
+
+function helpButton() {
+  textSize(16);
+  textFont("georgia");
+  fill("black");
+  rect(270, 587, 57, 30);
+  fill(215, 214, 212);
+  text("help", 282, 610);
+
+  if (help === true) {
+    fill(223, 160, 85);
+    rect(140, 140, 300, 300);
+    fill("black");
+    text(
+      "Verwende die Pfeiltasten nach oben,\n unten,links und rechts um Mario\n zu bewegen.",
+      140,
+      270
+    );
+  }
+}
+
+function startButton() {
+  if (begin === true) {
+    noStroke();
+    fill(195, 38, 22);
+    textSize(35);
+    text("Start", 254, 556);
+  } else {
+    fill(195, 38, 22);
+    textSize(35);
+    text("Start", 254, 556);
+  }
+}
+
+function gameOver() {
+  if (lost === true) {
+    fill(223, 160, 85);
+    rect(200, 200, 200, 200);
+    fill("white");
+    text("you lost", 240, 300);
+  }
+}
+
+function food() {
+  if (newfood === true) {
+    foodx = Math.floor(random(8, 32)) * 15;
+    foody = Math.floor(random(8, 32)) * 15;
+    newfood = false;
+  }
+  image(pilz, foodx, foody, 15, 15);
+}
+
+function eat() {
+  if (snakex[0] === foodx && snakey[0] === foody) {
+    newfood = true;
+    snakex.push(pox);
+    snakey.push(poy);
+    snakeFaster = true;
+    score = score + 1;
+    if (score > highscore) {
+      highscore = highscore + 1;
+      newhighscore = true;
+    }
+  }
+}
+
+function snake() {
+  for (i = 1; i < snakex.length; i++) {
+    //für snake von vorne bis hinten sollen rechts gezeichnet werden
+    fill("red");
+    noStroke();
+    image(mario, snakex[i], snakey[i], 15, 15); //i für verschiedenen Array-Stellen
+    image(mario, snakex[i], snakey[i], 15, 15);
+  }
+  image(mario, pox, poy, 15, 15);
+}
+
+function dead() {
+  let death = false;
+  if (
+    snakex[0] <= 100 ||
+    snakex[0] >= 490 ||
+    snakey[0] <= 100 ||
+    snakey[0] >= 490
+  ) {
+    //wenn snake den Rand berührt auf Anfang
+    snakex = [300];
+    snakey = [300];
+    direction = 0;
+    score = 0;
+    begin = true;
+    lost = true;
+    death = true;
+  }
+  return death;
+}
+
+function dead2() {
+  for (i = 1; i < snakex.length; i++) {
+    if (snakex[0] === snakex[i] && snakey[0] === snakey[i]) {
+      snakex = [300];
+      snakey = [300];
+      direction = 0;
+      score = 0;
+      begin = true;
+      lost = true;
+    }
+  }
+}
+
+function speed() {
+  time++;
+  if (time === 4) {
+    //draw läuft 4 mal durch bis snake sich erneut um 10 bewegt
+    time = 0;
+    move(direction);
+    if (zfaster === 20) {
+      //snake wird für 50 Durchgänge schneller, dann wieder normal
+      zfaster = 0;
+    }
+  }
+}
+
+function move(direction) {
+  if (begin == false) {
+    if (direction != 0) {
+      //wenn direction nicht null ist (snake also läuft)
+      pox = snakex[snakex.length - 1];
+      poy = snakey[snakey.length - 1];
+
+      for (i = snakex.length - 1; i > 0; i--) {
+        snakex[i] = snakex[i - 1];
+        snakey[i] = snakey[i - 1];
+      }
+    }
+
+    if (direction === 1) {
+      snakex[0] = snakex[0] - 15;
+    }
+    if (direction === 2) {
+      snakey[0] = snakey[0] - 15;
+    }
+    if (direction === 3) {
+      snakex[0] = snakex[0] + 15;
+    }
+    if (direction === 4) {
+      snakey[0] = snakey[0] + 15;
+    }
+  }
+}
+
+function scoretext() {
+  textFont("Georgia");
+  textStyle(BOLD);
+  noStroke();
+  textSize(30);
+  fill(252, 223, 163);
+  text("score: " + score, 120, 70);
+}
+
+function highscoretext() {
+  textFont("Georgia");
+  noStroke();
+  textSize(30);
+  fill(252, 223, 163);
+  text("highscore: " + highscore, 300, 70);
+}
+
+function draw() {
+  clear();
+  spielfeld();
+  snake();
+  dead();
+  dead2();
+  food();
+  eat();
+  scoretext();
+  gameOver();
+  highscoretext();
+  helpButton();
+  startButton();
+  speed();
+  //STEUERUNG
+  if (keyIsDown(37)) {
+    //zuweisung von direction
+    direction = 1; //links
+  }
+  if (keyIsDown(39)) {
+    direction = 3; //rechts
+  }
+  if (keyIsDown(38)) {
+    direction = 2; //oben
+  }
+  if (keyIsDown(40)) {
+    direction = 4; //unten
+  }
+
+  if (begin) {
+    if (
+      mouseIsPressed &&
+      mouseX > 250 &&
+      mouseX < 350 &&
+      mouseY > 520 &&
+      mouseY < 570
+    ) {
+      //start Button
+      directions = 1;
+      begin = false;
+      lost = false;
+      newhighscore = false;
+    }
+  }
+
+  if (
+    mouseIsPressed &&
+    mouseX > 270 &&
+    mouseX < 330 &&
+    mouseY > 590 &&
+    mouseY < 620
+  ) {
+    //help Button
+    help = true;
+  }
+}
+
+function mousePressed() {
+  if (helpButton) {
+    help = false;
+  }
+}
